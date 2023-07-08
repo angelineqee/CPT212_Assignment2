87d36256b9b2b72b449acba1f5ee916ea654d179
angelineqee <91053468+angelineqee@users.noreply.github.com>
2023-07-09 01:00:26 +0800

    Half-done GUI

--- FindPathDFS.java
+++ FindPathDFS.java
@@ -1,18 +1,19 @@
 import java.util.*;
 
 public class FindPathDFS{
-    static Vertex source = null;
-    static Vertex destination = null;
-    static int i;
-    static ArrayList<Edge> edges = new ArrayList<Edge>();
-    static ArrayList<Vertex> vertices = new ArrayList<Vertex>();
+    int i;
+    ArrayList<Edge> edges = new ArrayList<Edge>();
+    ArrayList<Vertex> vertices = new ArrayList<Vertex>();
+    Vertex source = null;
+    Vertex destination = null;
+    String path = " ";
 
-    public static void addEdge(Vertex source, Vertex destination) {
+    public void addEdge(Vertex source, Vertex destination) {
             edges.add(new Edge(source, destination));
         
     }
 
-    public static void printPath(int source) {
+    public void printPath(int source) {
         int temp = 0;
         for(int n=0; n<edges.size(); n++){
             if (edges.get(n).source.getValue()==source){
@@ -21,12 +22,15 @@ public class FindPathDFS{
         }
 
         for(int k=temp;k<edges.size(); k++){
-            System.out.println(edges.get(k).source.getName() + " ");
+            path += edges.get(k).source.getName();
+            path += " ";
         }
-        System.out.print(destination.getName());
+        path += destination.getName();
     }
 
-    public static void FindPath (Vertex v, Vertex d){
+    public void FindPath (Vertex v, Vertex d){
+        source = v;
+        destination = d;
         for (int x=0; x<vertices.size();x++){
             vertices.get(x).setVisited(0);
         }
@@ -40,7 +44,7 @@ public class FindPathDFS{
 
     }
 
-    public static boolean DFS (Vertex v, Vertex d){
+    public boolean DFS (Vertex v, Vertex d){
         v.setVisited(i++);
         for (int j=0; j<v.neighbours.size(); j++)
         {
@@ -61,58 +65,58 @@ public class FindPathDFS{
         return false;
     }
 
-    public static void main (String[] args){
-        final int vertexNum = 13;
-        String [] locations = {"Masjid", "CS School","Fajar","Eureka","DTSP","Library","Padang Kawad","Sg. Dua Gate","Subaidah","Bukit Gambir Gate","Swimming Pool","RST","Minden Gate"};
-        Vertex [] vertices = new Vertex[vertexNum];
-        for (int i=0; i<vertexNum; i++)
-        {
-            vertices[i] = new Vertex(i,locations[i]);
-        }
+    // public static void main (String[] args){
+        // final int vertexNum = 13;
+        // String [] locations = {"Masjid", "CS School","Fajar","Eureka","DTSP","Library","Padang Kawad","Sg. Dua Gate","Subaidah","Bukit Gambir Gate","Swimming Pool","RST","Minden Gate"};
+        // Vertex [] vertices = new Vertex[vertexNum];
+        // for (int i=0; i<vertexNum; i++)
+        // {
+        //     vertices[i] = new Vertex(i,locations[i]);
+        // }
 
-        vertices[0].addNeighbour(vertices[1]);
-        vertices[0].addNeighbour(vertices[2]);
-        vertices[0].addNeighbour(vertices[3]);
-        vertices[0].addNeighbour(vertices[4]);
-        vertices[0].addNeighbour(vertices[5]);
+        // vertices[0].addNeighbour(vertices[1]);
+        // vertices[0].addNeighbour(vertices[2]);
+        // vertices[0].addNeighbour(vertices[3]);
+        // vertices[0].addNeighbour(vertices[4]);
+        // vertices[0].addNeighbour(vertices[5]);
 
-        vertices[1].addNeighbour(vertices[0]);
-        vertices[1].addNeighbour(vertices[6]);
+        // vertices[1].addNeighbour(vertices[0]);
+        // vertices[1].addNeighbour(vertices[6]);
 
-        vertices[2].addNeighbour(vertices[0]);
-        vertices[2].addNeighbour(vertices[6]);
+        // vertices[2].addNeighbour(vertices[0]);
+        // vertices[2].addNeighbour(vertices[6]);
 
-        vertices[3].addNeighbour(vertices[0]);
-        vertices[3].addNeighbour(vertices[7]);
+        // vertices[3].addNeighbour(vertices[0]);
+        // vertices[3].addNeighbour(vertices[7]);
 
-        vertices[4].addNeighbour(vertices[0]);
-        vertices[4].addNeighbour(vertices[8]);
+        // vertices[4].addNeighbour(vertices[0]);
+        // vertices[4].addNeighbour(vertices[8]);
 
-        vertices[5].addNeighbour(vertices[0]);
+        // vertices[5].addNeighbour(vertices[0]);
 
-        vertices[6].addNeighbour(vertices[1]);
-        vertices[6].addNeighbour(vertices[2]);
-        vertices[6].addNeighbour(vertices[9]);
+        // vertices[6].addNeighbour(vertices[1]);
+        // vertices[6].addNeighbour(vertices[2]);
+        // vertices[6].addNeighbour(vertices[9]);
 
-        vertices[7].addNeighbour(vertices[3]);
+        // vertices[7].addNeighbour(vertices[3]);
 
-        vertices[8].addNeighbour(vertices[4]);
-        vertices[8].addNeighbour(vertices[10]);
+        // vertices[8].addNeighbour(vertices[4]);
+        // vertices[8].addNeighbour(vertices[10]);
 
-        vertices[9].addNeighbour(vertices[6]);
-        vertices[9].addNeighbour(vertices[11]);
+        // vertices[9].addNeighbour(vertices[6]);
+        // vertices[9].addNeighbour(vertices[11]);
 
-        vertices[10].addNeighbour(vertices[8]);
-        vertices[10].addNeighbour(vertices[12]);
+        // vertices[10].addNeighbour(vertices[8]);
+        // vertices[10].addNeighbour(vertices[12]);
 
-        vertices[11].addNeighbour(vertices[9]);
+        // vertices[11].addNeighbour(vertices[9]);
 
-        vertices[12].addNeighbour(vertices[10]);
+        // vertices[12].addNeighbour(vertices[10]);
 
-        source = vertices[12];
-        destination = vertices[11];
-        FindPath(source, destination);
+        // source = vertices[12];
+        // destination = vertices[11];
+        // FindPath(source, destination);
 
-    }
+    // }
 
 }
